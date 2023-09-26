cd4391ff8de1f0ef7bec5a39e76adb59543ab1f1
Ariel <a.t.navarro.1993@gmail.com>
2023-09-25 21:44:01 -0300

    agregada dependencias, modificado gitignore

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,9 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use rusqlite::{Connection};
+
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command(rename_all = "snake_case")]
 fn greet(name: &str,last_name: &str,age: &str) -> String {
@@ -8,6 +11,7 @@ fn greet(name: &str,last_name: &str,age: &str) -> String {
 }
 
 fn main() {
+    let _conn = Connection::open("./assets/caja.db");
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
